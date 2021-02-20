33673150d21025f004ca0925fbe6b8d8ccf1745f
theo-cerutti <theo@cerutti.com>
2021-02-20 19:09:38 +0100

    Add bulma + setup sass for use variable

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
-import './App.css';
+import './App.scss';
+import Button from 'react-bulma-components/lib/components/button';
 
 function App() {
   return (
     <div className="App">
+      <Button color='primary'>Press Me!</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
